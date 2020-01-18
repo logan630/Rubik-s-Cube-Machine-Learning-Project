26ea50622c3094ae9be22f437cfede58bfccc6ee
logan630 <43626179+logan630@users.noreply.github.com>
2020-01-18 14:26:00 -0500

    Code so far
    wrote cubie class. started rubikscube class

---
+++ Rubik's Cube Code.py
@@ -0,0 +1,41 @@
+class Cubie:
+
+    def __init__(self, x, y, z):
+
+        self.x = x
+        self.y = y
+        self.z = z
+
+        self.colors = {
+            'Up': 'White',
+            'Front': 'Green',
+            'Down': 'Yellow',
+            'Back': 'Blue',
+            'Left': 'Orange',
+            'Right': 'Red'
+        }
+
+    def __eq__(self, other):
+
+        return self.colors == other.colors
+
+    def check_equal_position(self, other):
+
+        correct_x = self.x == other.x 
+        correct_y = self.y == other.y 
+        correct_z = self.z == other.z 
+
+        return correct_x and correct_y and correct_z
+
+
+class RubiksCube:
+
+    def __init__(self):
+
+        self.components = []
+
+        for i in range(-1,2):
+            for j in range(-1,2):
+                for k in range(-1,2):
+
+                    self.components.append(Cubie(i,j,k))    
